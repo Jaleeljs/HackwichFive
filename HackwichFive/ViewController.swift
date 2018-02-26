b7de6e50d0c2066885f5eaff50695d5b5715f46f
CM Student <cmstudent@UHWO-ACM-18.local>
2018-02-26 10:51:22 -1000

    Made a table and cells and put data into each

--- HackwichFive/ViewController.swift
+++ HackwichFive/ViewController.swift
@@ -8,11 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    //define an array variable using an array literal with 5 items of type string
+ 
+    @IBOutlet weak var tableView: UITableView!
+    var colorarray = ["Red", "Blue", "Yellow", "Green", "Purple"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+//set VC as Delegate
+//self.tableView.datasource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 
+    return colorarray.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //set up cell to display items in colorarray
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        let text = colorarray[indexPath.row]
+        cell.textLabel?.text = text
+        return cell
+    }
 }
 
